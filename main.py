936ff5d697f326be43971df2283ae758724b097e
victorachen <vchen2120@gmail.com>
2022-06-21 22:15:49 -0700

    plugging away

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
+#work on renaming (line 142) for any of this to work!!
 #to do: incorporate tenant owned homes
 #to do: make this automatic on gmail (2 rent increase dates per year - automatically send the email)
 
 import csv, datetime, ezgmail, os
 from datetime import date, datetime
 from csv import writer
+os.chdir(r'C:\Users\Lenovo\PycharmProjects\rentincrease')
+ezgmail.init()
 
-rentincreasedate = '8/1/2022'
-path = r'C:\Users\Lenovo\PycharmProjects\rentincrease\venv\rentroll.csv'
-file = open(path)
-reader = csv.reader(file)
-data = list(reader)
-proplist = ['Holiday', 'Mt Vista', 'Westwind', 'Wilson Gardens', 'Crestview', \
-     'Hitching Post', 'Patrician', 'Wishing Well', 'SFH']
-# print(data)
+#code you put in the beginning? (I suck at this)
+def starter():
+    rentincreasedate = '8/1/2022'
+    path = r'C:\Users\Lenovo\PycharmProjects\rentincrease\venv\rentroll.csv'
+    file = open(path)
+    reader = csv.reader(file)
+    data = list(reader)
+    proplist = ['Holiday', 'Mt Vista', 'Westwind', 'Wilson Gardens', 'Crestview', \
+         'Hitching Post', 'Patrician', 'Wishing Well', 'SFH']
 
 #given a property (string), abbreviate it (or group it into broader category [like 'SFH'])
 def abbr_prop(longpropname):
@@ -121,7 +125,32 @@ def append_list_as_row(file_name, list_of_elem):
         # Add contents of list as last row in the csv file
         csv_writer.writerow(list_of_elem)
 
+#On June 1st, send an email attachment with the csv file
+def sendemail():
+    output = r'C:\Users\Lenovo\PycharmProjects\rentincrease\venv\output.csv'
+    organized_output = r'C:\Users\Lenovo\PycharmProjects\rentincrease\venv\organized_output.csv'
+    ezgmail.send('vchen2120@gmail.com','6/20 getting started','',[output,organized_output])
 
+#scrape victoreceipts@gmail.com for AppFolio's daily automated email
+def DownloadRentRoll():
+    resultsThreads = ezgmail.search('Automate Rent Increase')
+    mostrecentemail = resultsThreads[0]
+
+    #Download the csv into local directory
+    downloadfolder = r'C:\Users\Lenovo\PycharmProjects\rentincrease\venv'
+    mostrecentemail.messages[0].downloadAllAttachments(downloadFolder=downloadfolder)
+    #Name the most recently downloaded rent roll (what you did just above) --> "rentroll.csv"
+
+
+
+#Before we start anything, let's delete all previous files in local directory named "rentroll.csv"
+os.remove(r'C:\Users\Lenovo\PycharmProjects\rentincrease\venv\rentroll.csv')
+#Then, let's download a fresh rentroll from Appfolio
+DownloadRentRoll()
+
+#Now, we can start (the rest of) our code
+#------------------------------------------------------------------------------------------
+starter()
 # Create the output csv
 altered_data = alter(data)
 output_path = r'C:\Users\Lenovo\PycharmProjects\rentincrease\venv\output.csv'
@@ -130,10 +159,5 @@ NewCsv(altered_data, output_path)
 # Create the organized_output csv
 organize(altered_data)
 
-#On June 1st, send an email attachment with the csv file
-def sendemail():
-    os.chdir(r'C:\Users\Lenovo\PycharmProjects\rentincrease')
-    ezgmail.init()
-    ezgmail.send('vchen2120@gmail.com','6/20 getting started','')
-
-sendemail()
+#send the email to all prop managers
+# sendemail()
